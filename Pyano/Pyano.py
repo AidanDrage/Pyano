b5f7fe1ec653cec66136a8e69e493d6669e40ed1
Aidan <aidandrage@hotmail.com>
2020-08-22 13:50:36 +0100

    Code formatting changes

--- Pyano/Pyano.py
+++ Pyano/Pyano.py
@@ -5,28 +5,29 @@ import SetingsPage as sp
         
 class PyanoApp(tk.Tk):
 
+    global currentpage 
+    global listnerThread
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        #self.geometry("500x200")
-
         menubar = tk.Menu(self)
         menubar.add_command(label="Home", 
-            command = lambda: change_page(hp.HomePage(self)))
+            command = lambda: self.change_page(hp.HomePage(self)))
         menubar.add_command(label="Settings", 
-            command = lambda: change_page(sp.SettingsPage(self)))
+            command = lambda: self.change_page(sp.SettingsPage(self)))
         self.config(menu=menubar)
         
         global currentpage 
         currentpage = hp.HomePage(self)
         currentpage.pack()
 
-        def change_page(newpage):
-            global currentpage
-            currentpage.pack_forget()
+    def change_page(self, newpage):
+        global currentpage
+        currentpage.pack_forget()
             
-            currentpage = newpage
-            newpage.pack()
+        currentpage = newpage
+        newpage.pack()
 
 app = PyanoApp()
 app.mainloop()
